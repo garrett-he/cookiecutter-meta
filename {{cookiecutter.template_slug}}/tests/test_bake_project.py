ad362399d256ac3cc3ba29363c6f2b37ea88bee0
Garrett HE <garrett.he@outlook.com>
2023-01-11 10:37:24 +0800

    Project: Add configuration file for editors

--- {{cookiecutter.template_slug}}/tests/test_bake_project.py
+++ {{cookiecutter.template_slug}}/tests/test_bake_project.py
@@ -5,4 +5,4 @@ def test_create_project(cookies: Cookies):
     result = cookies.bake()
     assert not result.exception
 
-    assert result.project_path.exists()
+    assert result.project_path.joinpath('.editorconfig').exists()
